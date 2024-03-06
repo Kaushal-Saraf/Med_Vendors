5ac891c8dac5f91f4365b4a8716f66901dce8c22
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-06 11:28:11 +0530

    just ended got

--- Components/Header.js
+++ Components/Header.js
@@ -1,11 +1,12 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 const Header = () => {
   return (
     <div className="h-8 px-5 bg-blue-300 text-white text-center py-0.5">
-      <p className="font-extrabold font-mono text-xl" href="/">
+      <Link href="/" className="font-extrabold font-mono text-xl">
         Med_Vendors
-      </p>
+      </Link>
     </div>
   );
 };
--- app/Vendor/Vendorsignin/page.js
+++ app/Vendor/Vendorsignin/page.js
@@ -1,10 +1,20 @@
 "use client";
-import React from "react";
-
+import React, { useState } from "react";
 const vendorsignin = () => {
+  const [details, setdetails] = useState({
+    name:"",
+    nameVerifier:true,
+    contact:"",
+    contactVerifer:true,
+    password:"",
+    passwordVerifier:true
+  })
+  const handleSubmit=(e)=>{
+
+  }
   return (
     <>
-      <form className="w-[350px] pb-6 bg-white my-8 mx-auto rounded-lg shadow-sm">
+      <form onSubmit={handleSubmit} className="w-[350px] pb-6 bg-white my-8 mx-auto rounded-lg shadow-sm">
         <h1 className="text-center bg-blue-300 text-white font-bold rounded-t-lg shadow-sm">
           Vendor Signin Form
         </h1>
@@ -19,8 +29,31 @@ const vendorsignin = () => {
             autoComplete="given-name"
             placeholder="Raju Verma"
             className="flex-1 text-center mx-2 bg-blue-50 focus:outline-blue-400 text-blue-400"
+            onChange={(e)=>{
+              let name= e.target.value;
+              let flag= false;
+              for(let i=0; i<name.length; i++){
+                if(name[i]!=' ') flag=true;
+              }
+              if(!flag){
+                setdetails({
+                  ...details,
+                  name:name,
+                  nameVerifier:true
+                })
+              }
+              else{
+                setdetails({
+                  ...details,
+                  name:name,
+                  nameVerifier:false
+                })
+              }
+            }}
+            value={details.name}
           ></input>
         </div>
+        {details.nameVerifier? <div className="text-red-400 text-center mt-[-40px]">Please enter your name.</div> : null}
         <div className="flex my-12 px-2 w-full">
           <label htmlFor="contact" className="flex-1 text-center text-blue-400 font-semibold">
             Contact
@@ -30,23 +63,63 @@ const vendorsignin = () => {
             id="contact"
             name="contact"
             placeholder="9999999999"
+            autoComplete="tel-national"
             className="flex-1 text-center mx-2 bg-blue-50 focus:outline-blue-400 text-blue-400"
+            onChange={(e)=>{
+              let contact= e.target.value;
+              if(contact.length===10){
+                setdetails({
+                  ...details,
+                  contact:contact,
+                  contactVerifer:false
+                })
+              }
+              else{
+                setdetails({
+                  ...details,
+                  contact:contact,
+                  contactVerifer:true
+                })
+              }
+            }}
+            value={details.contact}
           ></input>
         </div>
+        {details.contactVerifer? <div className="text-red-400 text-center mt-[-40px]">Contact must contain 10 digits only.</div> : null}
         <div className="flex my-12 px-2 w-full">
           <label htmlFor="password" className="flex-1 text-center text-blue-400 font-semibold">
             Password
           </label>
           <input
-            type="text"
+            type="password"
             id="password"
             name="password"
             placeholder="*****"
             className="flex-1 text-center mx-2 bg-blue-50 focus:outline-blue-400 text-blue-400"
+            autoComplete="new-password"
+            onChange={(e)=>{
+              let password = e.target.value;
+              if(password.length>=8){
+                setdetails({
+                  ...details,
+                  password:password,
+                  passwordVerifier:false
+                })
+              }
+              else{
+                setdetails({
+                  ...details,
+                  password:password,
+                  passwordVerifier:true
+                })
+              }
+            }}
+            value={details.password}
           ></input>
         </div>
-        <div className="flex w-full justify-center">
-          <button className="text-center bg-blue-400 px-2 py-1 rounded text-white">
+        {details.passwordVerifier? <div className="text-red-400 text-center mt-[-40px]">Password must be greater than 8 digits.</div> : null}
+        <div className="flex w-full mt-10 justify-center">
+          <button type="submit" className="text-center bg-blue-400 px-2 py-1 rounded text-white">
             Send OTP
           </button>
         </div>
