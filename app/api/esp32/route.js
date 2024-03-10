7ad7237ae78ac2a734d4d33a4602ddbfb3c9f683
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-10 16:27:38 +0530

    hello esp

--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -3,5 +3,5 @@
 const { NextResponse } = require("next/server");
 export async function POST(req) {
   console.log(await req.json());
-  return NextResponse.json("hello");
+  return NextResponse.json({"hello":  "world"});
 }
