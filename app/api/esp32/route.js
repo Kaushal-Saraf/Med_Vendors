bf2e64f74b85fd9efb24b5c36121c8eb722c1a27
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-12 14:45:46 +0530

    trying to see the image

--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req){
-  const data = await req.formData();
-  const id = data.get('image');
-  console.log(id);
-  return NextResponse.json({message: 'ok'});
+  const imageBuffer = await req.body;
+  console.log(imageBuffer.length)
+  return NextResponse.json({message: imageBuffer.length});
 }
