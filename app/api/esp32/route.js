f1d510cd7abff35cd1ad467ea7563b2e07510bba
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-12 17:05:26 +0530

    finally sucessfully sended the image to esp

--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -2,6 +2,5 @@ import { NextResponse } from "next/server";
 
 export async function POST(req){
   const data = await req.formData();
-  console.log(data.get('image').name); 
-  return NextResponse.json({message: data.name});
+  return NextResponse.json({message: data.get('image').name});
 }
