bee6667bfab12355f0ebaf11618d0d6424e08a72
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-12 16:35:39 +0530

    trying making qr code

--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -3,5 +3,5 @@ import { NextResponse } from "next/server";
 export async function POST(req){
   const data = await req.formData();
   console.log(data);
-  return NextResponse.json({message: "helo"});
+  return NextResponse.json({message: data.name});
 }
