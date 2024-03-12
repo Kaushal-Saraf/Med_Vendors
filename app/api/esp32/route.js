fba93e42d8d7c987f115b520e328aea3a3f41154
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-12 13:32:52 +0530

    getting image form esp

--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -1,27 +1,8 @@
-import formidable from 'formidable';
-import fs from 'fs';
-import path from 'path';
+import { NextResponse } from "next/server";
 
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async function handler(req, res) {
-  const form = new formidable.IncomingForm();
-  form.uploadDir = './public/uploads';
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      res.status(500).json({ error: 'Error occurred while uploading image' });
-      return;
-    }
-    const tempFilePath = files.image.path;
-    const uniqueFileName = `${Date.now()}-${files.image.name}`;
-    const destinationPath = path.join(form.uploadDir, uniqueFileName);
-    fs.renameSync(tempFilePath, destinationPath);
-    const imageUrl = `/uploads/${uniqueFileName}`;
-    res.status(200).json({ imageUrl });
-  });
+export async function POST(req){
+  const data = await req.formData();
+  const id = data.get('image');
+  console.log(id);
+  return NextResponse.json({message: 'ok'});
 }
