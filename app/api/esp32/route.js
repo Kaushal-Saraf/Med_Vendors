9483ccca1c38dc6a107c19240379a7acdafb655e
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-15 13:58:21 +0530

    qr code scanner

--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
-
 export async function POST(req){
   const data = await req.formData();
-  console.log(data);
-  return NextResponse.json({message: data.get('image').name});
+  const image = data.get('image');
+  const buffer = await image.arrayBuffer();
+  console.log(buffer)
+  console.log(image)
+  return NextResponse.json({message: data});
 }
