3157abe92ab433e8671f26b63459d3c73b430be8
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-15 14:32:02 +0530

    qr code scanner

--- Services/qrCodeDetector.js
+++ Services/qrCodeDetector.js
@@ -1,13 +1,24 @@
 import jsQR from 'jsqr';
 
-export async function qrCodeDetector(base64Image) {
-    return new Promise((resolve, reject) => {
-        const buffer = Buffer.from(base64Image, 'base64');
-        const code = jsQR(buffer, buffer.length);
-        if (code) {
-            resolve(code.data);
-        } else {
-            reject(new Error('No QR code found in the image.'));
-        }
-    });
+export async function qrCodeDetector(buffer) {
+    const imageData = buffer;
+    const width = 640;
+    const height = 480;
+    const grayPixels = new Uint8ClampedArray(width * height);
+    for (let i = 0; i < imageData.length; i += 4) {
+        const grayValue = imageData[i] * 0.2126 + imageData[i + 1] * 0.7152 + imageData[i + 2] * 0.0722;
+        grayPixels[i / 4] = grayValue;
+    }
+    const code = {
+        data: grayPixels,
+        width: width,
+        height: height
+    };
+    console.log(code)
+    // const codeResult = jsQR(code.data, code.width, code.height);
+    // if (codeResult) {
+    //     console.log("QR Code detected with data: ", codeResult.data);
+    // } else {
+    //     console.log("No QR Code detected.");
+    // }
 }
--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -1,9 +1,9 @@
+import { qrCodeDetector } from "@/Services/qrCodeDetector";
 import { NextResponse } from "next/server";
 export async function POST(req){
   const data = await req.formData();
+  console.log(data.get('image'))
   const image = data.get('image');
-  const buffer = await image.arrayBuffer();
-  console.log(buffer)
-  console.log(image)
+  qrCodeDetector(image);
   return NextResponse.json({message: data});
 }
