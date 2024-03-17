bdbb884083457535de0c417da62fcae72a9c4be5
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-17 11:03:17 +0530

    tyna makna scanna

--- Services/qrCodeDetector.js
+++ Services/qrCodeDetector.js
@@ -1,20 +1,16 @@
 import jsQR from 'jsqr';
+import fs from 'fs';
+export async function qrCodeDetector(file) {
+    const width = 640;
+    const height = 480;
+    
 
-export async function qrCodeDetector(buffer) {
-    const imageData = buffer;
-    console.log(imageData);
-    const width = 800;
-    const height = 600;
-    const grayPixels = new Uint8ClampedArray(width * height);
-    for (let i = 0; i < imageData.length; i += 4) {
-        const grayValue = imageData[i] * 0.2126 + imageData[i + 1] * 0.7152 + imageData[i + 2] * 0.0722;
-        grayPixels[i / 4] = grayValue;
-    }
-    const code = {
-        data: grayPixels,
-        width: width,
-        height: height
-    };
+const readFileInBase64 = async (file) => {
+  const base64String = Buffer.from(fileBuffer).toString('base64');
+  return base64String;
+};
+
+const base64String = await readFileInBase64('my-file.txt');
     try{
         const codeResult = jsQR(code.data, code.width, code.height);
         return codeResult;
--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -2,8 +2,6 @@ import { qrCodeDetector } from "@/Services/qrCodeDetector";
 import { NextResponse } from "next/server";
 export async function POST(req){
   const data = await req.formData();
-  console.log(data.get('image'))
-  const image = data.get('image');
-  qrCodeDetector(image);
+  console.log(data.get('image'));
   return NextResponse.json({message: data});
 }
