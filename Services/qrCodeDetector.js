e94ae6efdcdcefa4d62ae5e82f4903dede068306
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-17 10:08:59 +0530

    tyna makna scanna

--- Services/qrCodeDetector.js
+++ Services/qrCodeDetector.js
@@ -2,8 +2,9 @@ import jsQR from 'jsqr';
 
 export async function qrCodeDetector(buffer) {
     const imageData = buffer;
-    const width = 640;
-    const height = 480;
+    console.log(imageData);
+    const width = 800;
+    const height = 600;
     const grayPixels = new Uint8ClampedArray(width * height);
     for (let i = 0; i < imageData.length; i += 4) {
         const grayValue = imageData[i] * 0.2126 + imageData[i + 1] * 0.7152 + imageData[i + 2] * 0.0722;
@@ -14,11 +15,12 @@ export async function qrCodeDetector(buffer) {
         width: width,
         height: height
     };
-    console.log(code)
-    // const codeResult = jsQR(code.data, code.width, code.height);
-    // if (codeResult) {
-    //     console.log("QR Code detected with data: ", codeResult.data);
-    // } else {
-    //     console.log("No QR Code detected.");
-    // }
+    try{
+        const codeResult = jsQR(code.data, code.width, code.height);
+        return codeResult;
+    }
+    catch(e){
+        console.log(e);
+        return "NO QR Found";
+    }
 }
