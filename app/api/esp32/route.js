481d61124d613bffe6913b58d51223023bb2e3a7
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-17 11:26:53 +0530

    tyna makna scanna

--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -2,6 +2,6 @@ import { qrCodeDetector } from "@/Services/qrCodeDetector";
 import { NextResponse } from "next/server";
 export async function POST(req){
   const data = await req.body;
-  console.log(data);
+  console.log(data.image);
   return NextResponse.json({message: data});
 }
