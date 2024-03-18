7a08475c992822a014032fc4ae757f3d80d1b142
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-18 10:33:54 +0530

    tyna makna scanna

--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -4,6 +4,6 @@ import { NextResponse } from "next/server";
 import { Stream } from 'stream';
 export async function POST(req){
   const data = await req.json();
-  console.log(data);
+  console.log(data.image[0] + data.image[1] + data.image[image.length-2] + data.image[image.length - 1]);
   return NextResponse.json({message: "hwlo"});
 }
