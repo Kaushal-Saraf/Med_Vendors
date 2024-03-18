b8e7890d013092ec45a6f0e4564b3980014a186c
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-18 08:34:03 +0530

    tyna makna scanna

--- app/api/esp32/route.js
+++ app/api/esp32/route.js
@@ -3,6 +3,6 @@ import { promisify } from 'util';
 import { NextResponse } from "next/server";
 export async function POST(req){
   const data = await req.body;
-  console.log(data.image);
+  console.log(data.image[0] + data.image[image.length -1]);
   return NextResponse.json({message: "hwlo"});
 }
