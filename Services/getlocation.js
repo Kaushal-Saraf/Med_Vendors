8ff5fea0eac3c188a969557f25c8e506454f086d
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-04-06 14:11:35 +0530

    Hello

--- Services/getlocation.js
+++ Services/getlocation.js
@@ -1,15 +1,21 @@
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
 export default function getLocation() {
   return new Promise((resolve, reject) => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
-        function(position) {
+        function (position) {
           const latitude = position.coords.latitude;
           const longitude = position.coords.longitude;
           resolve({ Latitude: latitude, Longitude: longitude });
         },
-        function(error) {
+        function (error) {
           reject({ error: error });
-        }
+        },
+        options
       );
     } else {
       reject({ error: "Geolocation is not supported" });
