92dab614abd71e5cc644f1f8384ed0afdca43d86
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-05-01 08:39:37 +0530

    on the verge of completeing vendor

--- models/machine.js
+++ models/machine.js
@@ -1,63 +1,69 @@
 const { default: mongoose, Schema } = require("mongoose");
 
-const machineSchema=new Schema({
-    //add before selling a machine
-    umid:{
-        type:String,
-        unique:true
+const machineSchema = new Schema({
+  //add before selling a machine
+  umid: {
+    type: String,
+    unique: true,
+  },
+  noofslots: {
+    type: Number,
+    unique: true,
+  },
+  //add after selling of machine
+  ownerid: {
+    type: String,
+  },
+  address: {
+    type: {
+      address: {
+        type: String,
+      },
+      longitude: {
+        type: Number,
+      },
+      latitude: {
+        type: Number,
+      },
     },
-    noofslots:{
-        type:Number,
-        unique:true,
-    },
-    //add after selling of machine
-    ownerid:{
-        type:String,
-    },
-    address:{
-        type:{
-            address:{
-                type:String,
-            },
-            longitude:{
-                type:Number,
-            },
-            latitude:{
-                type:Number,
-            }
+  },
+  medicinedetails: {
+    type: [
+      {
+        slot: {
+          type: Number,
         },
-    },
-    medicinedetails:{
-        type:[{
-            slot:{
-                type:Number,
+        name: {
+          type: String,
+        },
+        dosage: {
+          type: Number,
+        },
+        cpsuleeachpack: {
+          type: Number,
+        },
+        expiry: {
+          type: Date,
+        },
+        price: {
+          type: Number,
+        },
+        noofpack: {
+          type:{
+            sold: {
+              type: Number,
             },
-            name:{
-                type:String,
+            soldandnotcollected: {
+                type: Number,
             },
-            dosage:{
-                type:Number,
+            notsold: {
+              type: Number,
             },
-            cpsuleeachpack:{
-                type:Number,
-            },
-            medicines:{
-                    type:[{
-                        
-                        expiry:{
-                            type:Date,
-                        },
-                        price:{
-                            type:Number,
-                        },
-                        status:{
-                            type:String,
-                        }
-                    }]
-                }
-            }
-        ]
-    }
-})
-export const machine=
- mongoose.models.machine|| mongoose.model("machine",machineSchema)
+          }
+        },
+      },
+    ],
+  },
+});
+export const machine =
+  mongoose.models.machine || mongoose.model("machine", machineSchema);
