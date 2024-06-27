2f36f078da6834ee5fb8f57cb94a641b78035d1f
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-06-27 13:28:06 +0530

    hello

--- app/api/machine/[id]/addmedicine/route.js
+++ app/api/machine/[id]/addmedicine/route.js
@@ -6,20 +6,21 @@ export async function PATCH(req, { params }) {
   await connectDb();
   const data = await req.json();
   const newmedicine = {
-    slot: data.slot,
+    slot: Number(data.slot),
     name: data.medicinename,
     // dosage: data.dosage,
-    cpsuleeachpack: data.capsuleeachpack,
+    cpsuleeachpack: Number(data.capsuleeachpack),
     expiry: data.expiry,
-    price: data.price,
+    price: Number(data.price),
     sold: 0,
     soldandnotcollected: 0,
-    notsold: data.quantity,
+    notsold: Number(data.quantity),
   };
+
   const machinedetails = await machine.findOne({ umid: params.id });
   const prevmedicine = machinedetails.medicinedetails;
   const slots = prevmedicine.map((medicine) => medicine.slot);
-  if (slots.includes(data.slot)) {
+  if (slots.includes(newmedicine.slot)) {
     await machine.updateOne(
       { umid: params.id },
       { $pull: { medicinedetails: { slot: newmedicine.slot } } }
